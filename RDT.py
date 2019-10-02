ce84e7dea9b856b281eb6536a7bc0395cc1a2bfe
Nick <nicktonjum@me.com>
2019-10-02 13:38:03 -0600

    long ack and nak corrupt method

--- RDT.py
+++ RDT.py
@@ -8,12 +8,15 @@ class Packet:
     ## the number of bytes used to store packet length
     seq_num_S_length = 10
     length_S_length = 10
+    ack_nak_length = 10
     ## length of md5 checksum in hex
     checksum_length = 32 
         
-    def __init__(self, seq_num, msg_S):
+    def __init__(self, seq_num, msg_S, ack=0, nak=0):
         self.seq_num = seq_num
         self.msg_S = msg_S
+        self.ack = ack
+        self.nak = nak
         
     @classmethod
     def from_byte_S(self, byte_S):
@@ -21,20 +24,29 @@ class Packet:
             raise RuntimeError('Cannot initialize Packet: byte_S is corrupt')
         #extract the fields
         seq_num = int(byte_S[Packet.length_S_length : Packet.length_S_length+Packet.seq_num_S_length])
-        msg_S = byte_S[Packet.length_S_length+Packet.seq_num_S_length+Packet.checksum_length :]
-        return self(seq_num, msg_S)
+        #ack and nak
+        ack = int(byte_S[Packet.length_S_length+Packet.seq_num_S_length+Packet.checksum_length :
+                         Packet.length_S_length+Packet.seq_num_S_length+Packet.checksum_length+Packet.ack_nak_length])
+        nak = int(byte_S[Packet.length_S_length+Packet.seq_num_S_length+Packet.checksum_length+Packet.ack_nak_length :
+                    Packet.length_S_length+Packet.seq_num_S_length+Packet.checksum_length+Packet.ack_nak_length+Packet.ack_nak_length])
+        msg_S = byte_S[Packet.length_S_length+Packet.seq_num_S_length+Packet.checksum_length+(2*Packet.ack_nak_length) :]
+        return self(seq_num, msg_S, ack, nak)
         
         
     def get_byte_S(self):
         #convert sequence number of a byte field of seq_num_S_length bytes
         seq_num_S = str(self.seq_num).zfill(self.seq_num_S_length)
+        #ack and nak flags
+        ack_S = = str(self.ack).zfill(self.ack_nak_length)
+        nak_S = = str(self.nak).zfill(self.ack_nak_length)
         #convert length to a byte field of length_S_length bytes
-        length_S = str(self.length_S_length + len(seq_num_S) + self.checksum_length + len(self.msg_S)).zfill(self.length_S_length)
+        length_S = str(self.length_S_length + len(seq_num_S) + self.checksum_length + len(ack_S) + len(nak_S) +
+                                   len(self.msg_S)).zfill(self.length_S_length)
         #compute the checksum
-        checksum = hashlib.md5((length_S+seq_num_S+self.msg_S).encode('utf-8'))
+        checksum = hashlib.md5((length_S+seq_num_S+ack_S+nak_S+self.msg_S).encode('utf-8'))
         checksum_S = checksum.hexdigest()
         #compile into a string
-        return length_S + seq_num_S + checksum_S + self.msg_S
+        return length_S + seq_num_S + checksum_S + ack_S + nak_S + self.msg_S
    
     
     @staticmethod
@@ -42,6 +54,11 @@ class Packet:
         #extract the fields
         length_S = byte_S[0:Packet.length_S_length]
         seq_num_S = byte_S[Packet.length_S_length : Packet.seq_num_S_length+Packet.seq_num_S_length]
+        #ack and nak
+        ack_S = int(byte_S[Packet.length_S_length+Packet.seq_num_S_length+Packet.checksum_length :
+                         Packet.length_S_length+Packet.seq_num_S_length+Packet.checksum_length+Packet.ack_nak_length])
+        nak_S = int(byte_S[Packet.length_S_length+Packet.seq_num_S_length+Packet.checksum_length+Packet.ack_nak_length :
+                    Packet.length_S_length+Packet.seq_num_S_length+Packet.checksum_length+Packet.ack_nak_length+Packet.ack_nak_length])
         checksum_S = byte_S[Packet.seq_num_S_length+Packet.seq_num_S_length : Packet.seq_num_S_length+Packet.length_S_length+Packet.checksum_length]
         msg_S = byte_S[Packet.seq_num_S_length+Packet.seq_num_S_length+Packet.checksum_length :]
         
